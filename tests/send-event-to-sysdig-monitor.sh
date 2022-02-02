e7ed867deae85b1b3be8269d74d98a15ef5a4ebf
smazzone <stefano.mazzone@datadoghq.com>
2022-02-02 16:34:27 +0100

    cleanup

--- tests/send-event-to-sysdig-monitor.sh
+++ tests/send-event-to-sysdig-monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SDC_ACCESS_TOKEN='c09cefa5-7bd7-4b87-a430-ccee645a5884'
+SDC_ACCESS_TOKEN='YOUR-SYSDIG-ACCESS-TOKEN'
 ENDPOINT='eu1.app.sysdig.com'
 
 curl -X POST -s https://'"${ENDPOINT}"'/api/v2/events -H 'Content-Type: application/json; charset=UTF-8' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H "Authorization: Bearer ${SDC_ACCESS_TOKEN}" --data-binary '{"event": {"name": "Jenkins - start wordpress deploy", "description": "deploy", "severity": "MEDIUM", "scope": "host.hostName = \"ip-10-1-1-1\" and build = \"89\""}}}'
